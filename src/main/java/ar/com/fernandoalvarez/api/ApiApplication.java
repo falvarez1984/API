8fee08c648f2d1ee56a3e69679974504c6701401
falvarezSancor <fernando.alvarez@sancorsalud.com.ar>
2022-09-14 11:05:48 -0300

    Se agrega Swagger al proyecto

--- src/main/java/ar/com/fernandoalvarez/api/ApiApplication.java
+++ src/main/java/ar/com/fernandoalvarez/api/ApiApplication.java
@@ -1,13 +1,42 @@
 package ar.com.fernandoalvarez.api;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "BearerAuthentication";
+
+        return new OpenAPI().info(new Info().title("API").version("v1.0.0")
+                        .description("Proyecto API")
+                        .contact(new Contact().name("Fernando Alvarez").email("alvarezfernandonicolas@gmail.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)).components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
 
 }
