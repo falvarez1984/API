bbc4681dc0e76691df6539abeade180cefef449b
falvarezSancor <fernando.alvarez@sancorsalud.com.ar>
2022-09-14 11:41:22 -0300

    Se implementa Swagger en servicio

--- src/main/java/ar/com/fernandoalvarez/api/controller/UsuarioController.java
+++ src/main/java/ar/com/fernandoalvarez/api/controller/UsuarioController.java
@@ -1,12 +1,26 @@
 package ar.com.fernandoalvarez.api.controller;
 
 import ar.com.fernandoalvarez.api.dto.response.UsuarioResponseDto;
+import ar.com.fernandoalvarez.api.exception.ErrorDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
+@Tag(name = "Usuarios", description = "CRUD de usuarios")
 public interface UsuarioController {
 
+    @Operation(summary = "Consultar todos los usuarios.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Los usuarios se consultaron exitosamente", content = @Content(schema = @Schema(implementation = UsuarioResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "La petición es inválida", content = @Content(schema = @Schema(implementation = ErrorDetails.class))),
+            @ApiResponse(responseCode = "404", description = "Error al consultar usuarios", content = @Content(schema = @Schema(implementation = ErrorDetails.class))),
+            @ApiResponse(responseCode = "500", description = "Error interno al procesar la respuesta", content = @Content(schema = @Schema(implementation = ErrorDetails.class)))})
     public ResponseEntity<List<UsuarioResponseDto>> obtenerTodos();
 
 }
